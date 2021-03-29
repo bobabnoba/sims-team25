63062ea7a16a130b2b13457eddeca53fd00d4b4d
markovic-dusan <shandusukisuki3@gmail.com>
2021-03-29 20:25:53 +0200

    dizajn

---
+++ ZdravoKorporacija/ZdravoKorporacija/Stranice/LekarCRUD/otkaziPregled.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ZdravoKorporacija.Stranice.LekarCRUD
+{
+    /// <summary>
+    /// Interaction logic for otkaziPregled.xaml
+    /// </summary>
+    public partial class otkaziPregled : Window
+    {
+        public otkaziPregled()
+        {
+            InitializeComponent();
+        }
+
+        private void da(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ne(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
