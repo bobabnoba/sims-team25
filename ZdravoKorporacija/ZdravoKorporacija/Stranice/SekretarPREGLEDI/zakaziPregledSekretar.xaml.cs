711135be66a2ca6a547ce96253ac9f9bf8f3c8c2
markovic-dusan <shandusukisuki3@gmail.com>
2021-04-21 02:57:37 +0200

    zakazivanje ispravke

---
+++ ZdravoKorporacija/ZdravoKorporacija/Stranice/SekretarPREGLEDI/zakaziPregledSekretar.xaml.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Model;
+using Service;
+using ZdravoKorporacija.Model;
+
+namespace ZdravoKorporacija.Stranice.SekretarPREGLEDI
+{
+    /// <summary>
+    /// Interaction logic for zakaziPregledSekretar.xaml
+    /// </summary>
+    public partial class zakaziPregledSekretar : Window
+    {
+        private TerminService ts = new TerminService();
+
+        private ProstorijaService prostorijeStorage = new ProstorijaService();
+        private PacijentRepozitorijum pacijentiDat = new PacijentRepozitorijum();
+        private PacijentService pacijentiStorage = new PacijentService();
+        private List<Pacijent> pacijenti = new List<Pacijent>();
+        private LekarRepozitorijum lekariDat = new LekarRepozitorijum();
+        private List<Lekar> lekari = new List<Lekar>();
+        private List<Lekar> slobodniLekari;
+        private List<Prostorija> prostorije = new List<Prostorija>();
+
+        private Termin p;
+        private ObservableCollection<Termin> pregledi;
+
+        private Dictionary<int, int> ids = new Dictionary<int, int>();
+
+        DateTime dateTime = DateTime.Now;
+        public zakaziPregledSekretar(ObservableCollection<Termin> termini, Dictionary<int, int> ids)
+        {
+            InitializeComponent();
+
+            p = new Termin();
+            pacijenti = pacijentiDat.dobaviSve();
+            cbPacijent.ItemsSource = pacijenti;
+            pregledi = termini;
+
+
+            lekari = lekariDat.dobaviSve();
+            Lekari.ItemsSource = lekari;
+
+            slobodniLekari = lekari;
+            Lekari.ItemsSource = slobodniLekari;
+
+            this.ids = ids;
+            DateTime danas = DateTime.Today;
+
+            for (DateTime tm = danas.AddHours(8); tm < danas.AddHours(22); tm = tm.AddMinutes(30))
+            {
+                time.Items.Add(tm.ToShortTimeString());
+
+            }
+
+            CalendarDateRange cdr = new CalendarDateRange(DateTime.MinValue, DateTime.Today.AddDays(-1));
+            date.BlackoutDates.Add(cdr);
+
+
+
+
+
+
+
+            prostorije = prostorijeStorage.PregledSvihProstorija();
+            cbProstorija.ItemsSource = prostorije;
+            p.Trajanje = 0.5;
+
+
+            cbPacijent.IsEnabled = false;
+            cbProstorija.IsEnabled = false;
+            Lekari.IsEnabled = false;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            cbPacijent.IsEnabled = true;
+            cbProstorija.IsEnabled = true;
+            Lekari.IsEnabled = true;
+
+
+
+
+
+
+
+        }
+
+        private void potvrdi(object sender, RoutedEventArgs e)
+        {
+            int id = 0;
+            for (int i = 0; i < 1000; i++)
+            {
+                if (ids[i] == 0)
+                {
+                    id = i;
+                    ids[i] = 1;
+                    break;
+                }
+            }
+            p.Id = id;
+            p.Pocetak = DateTime.Parse(date.Text + " " + time.SelectedItem.ToString());
+            Pacijent pac = (Pacijent)cbPacijent.SelectedItem;
+
+
+            p.prostorija = (Prostorija)cbProstorija.SelectedItem;
+            p.Lekar = (Lekar)Lekari.SelectedItem;
+            if (pac.ZdravstveniKarton != null)
+                p.zdravstveniKarton = pac.ZdravstveniKarton;
+            else
+            {
+                p.zdravstveniKarton = new ZdravstveniKarton(null, pacijenti.Count + 1, StanjePacijentaEnum.None, null, KrvnaGrupaEnum.None, null);
+                pac.ZdravstveniKarton = new ZdravstveniKarton(null, pacijenti.Count + 1, StanjePacijentaEnum.None, null, KrvnaGrupaEnum.None, null);
+            }
+
+            Termin tZaLjekara = new Termin();
+            tZaLjekara.Id = p.Id;
+            p.Lekar.AddTermin(tZaLjekara);
+
+            if (ts.ZakaziTermin(p, ids))
+            {
+                this.pregledi.Add(p);
+                lekariDat.sacuvaj(lekari);
+
+            }
+            pac.AddTermin(p);
+            pacijentiStorage.AzurirajPacijenta(pac);
+            this.Close();
+        }
+
+        private void time_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void odustani(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void time_changed(object sender, SelectionChangedEventArgs e)
+        {
+            if (time.SelectedIndex != -1)
+            {
+                p.Pocetak = DateTime.Parse(date.Text + " " + time.SelectedItem.ToString());
+
+
+
+
+                foreach (Termin t in pregledi)
+                {
+                    if (t.Pocetak.Equals(p.Pocetak))
+                    {
+                        foreach (Lekar l in lekari.ToArray())
+                        {
+                            if (l.Jmbg.Equals(t.Lekar.Jmbg))
+                            {
+                                slobodniLekari.Remove(l);
+                            }
+                        }
+                    }
+                }
+
+                Lekari.IsEnabled = true;
+            }
+        }
+    }
+}
