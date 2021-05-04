926cfd7df78efa85ee43c3c868bf9a14fd56d2b3
markovic-dusan <shandusukisuki3@gmail.com>
2021-05-04 17:23:51 +0200

    cleancode

--- ZdravoKorporacija/ZdravoKorporacija/Service/TerminService.cs
+++ ZdravoKorporacija/ZdravoKorporacija/Service/TerminService.cs
@@ -2,6 +2,7 @@
 using Repository;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
 
 namespace ZdravoKorporacija.Model
@@ -108,6 +109,100 @@ namespace ZdravoKorporacija.Model
             TerminRepozitorijum datoteka = new TerminRepozitorijum();
             List<Termin> termini = datoteka.dobaviSve();
             return termini;
+        }
+
+      
+
+        public Termin InicijalizujTermin(int id, TipTerminaEnum tip,  DateTime pocetak, Pacijent pacijent, Lekar lekar, Prostorija prostorija)
+        {
+            Termin noviTermin = new Termin();
+            noviTermin.Id = id;
+            noviTermin.Tip = tip;
+            noviTermin.Pocetak = pocetak;
+            noviTermin.Lekar = lekar;
+            noviTermin.zdravstveniKarton = pacijent.ZdravstveniKarton;
+            noviTermin.prostorija = prostorija;
+            noviTermin.hitno = false;
+
+            return noviTermin;
+        }
+
+        public int MapaTermina(Dictionary<int, int> ids)
+        {
+            int id = 0;
+            for (int i = 0; i < 1000; i++)
+            {
+                if (ids[i] == 0)
+                {
+                    id = i;
+                    ids[i] = 1;
+                    break;
+                }
+            }
+            return id;
+        }
+        
+        public List<Lekar> DobaviSlobodneLekare(List<Lekar> lekari, ObservableCollection<Termin> pregledi, DateTime pocetakTermina)
+        {
+            List<Lekar> slobodniLekari = lekari;
+
+            foreach (Termin t in pregledi)
+            {
+                if (t.Pocetak.Equals(pocetakTermina))
+                {
+                    foreach (Lekar l in lekari.ToArray())
+                    {
+                        if (l.Jmbg.Equals(t.Lekar.Jmbg))
+                        {
+                            slobodniLekari.Remove(l);
+
+                        }
+                    }
+                }
+            }
+            return slobodniLekari;
+        }
+
+        public List<Prostorija> DobaviSlobodneProstorije(List<Prostorija> prostorije, ObservableCollection<Termin> pregledi, Termin termin)
+        {
+            List<Prostorija> slobodneProstorije = prostorije;
+
+            foreach (Termin t in pregledi)
+            {
+                if (t.Pocetak.Equals(termin.Pocetak))
+                {
+                    foreach (Prostorija p in prostorije.ToArray())
+                    {
+                        if (t.prostorija.Id.Equals(p.Id))
+                        {
+                            slobodneProstorije.Remove(p);
+                        }
+                    }
+                }
+            }
+            return slobodneProstorije;
+        }
+
+        public void InicijalizujTerminLekaru(Termin t)
+        {
+            Termin terminZaLekara = new Termin();
+            terminZaLekara.Id = t.Id;
+            t.Lekar.AddTermin(terminZaLekara);
+        }
+
+        public ZdravstveniKarton ProveriKartonKodZakazivanja(Pacijent pacijent)
+        {
+            ZdravstveniKarton kartonTermina = new ZdravstveniKarton();
+
+            if (pacijent.ZdravstveniKarton != null)
+                kartonTermina = pacijent.ZdravstveniKarton;
+            else
+            {
+                kartonTermina = new ZdravstveniKarton(null, pacijent.GetJmbg(), StanjePacijentaEnum.None, null, KrvnaGrupaEnum.None, null);
+                pacijent.ZdravstveniKarton = kartonTermina;
+            }
+
+            return kartonTermina;
         }
     }
 }
